83fc79db5bf17b2cc70f1dc8a2be4f31cef369c5
Bravenrl <bravenrl@gmail.com>
2022-10-13 02:30:21 +0300

    add connection line styles

--- src/components/table/svg-cell/svg-cell.tsx
+++ src/components/table/svg-cell/svg-cell.tsx
@@ -5,14 +5,20 @@ import { ReactComponent as FileSvg } from '../../../assets/icons/file.svg';
 type SvgCellProps = {
   isRow: boolean;
   parent: number | null;
+  level: number;
 };
 
-function SvgCell({ isRow, parent }: SvgCellProps): JSX.Element {
+function SvgCell({ isRow, parent, level }: SvgCellProps): JSX.Element {
   const margin = parent ? 24 * parent : 0;
+  const position = parent ? level - parent : 0;
+  const height = 55*position;
+  const width = parent? 12 : 0;
 
   return (
     <td>
       <div className={styles.icon} style={{ marginLeft: margin }}>
+        <div className={styles.vertical} style={{height}}></div>
+        <div className={styles.horizontal} style={{width}}></div>
         {isRow ? (
           <FileSvg />
         ) : (
--- src/components/table/table-body/table-body.tsx
+++ src/components/table/table-body/table-body.tsx
@@ -6,11 +6,11 @@ import styles from './table-body.module.scss';
 function TableBody(): JSX.Element {
   return (
     <tbody className={styles.tableBody}>
-      {data.map((row) => {
+      {data.map((row, index) => {
         const isRow = row.type === 'row'
         return (
           <tr key={row.id}>
-            <SvgCell parent={row.parent} isRow={isRow}/>
+            <SvgCell parent={row.parent} isRow={isRow} level={++index}/>
             <TableCell value={row.title} isEditable />
             <TableCell value={isRow?row.unit:''} />
             <TableCell value={isRow?row.quantity:''} isEditable={isRow} />
